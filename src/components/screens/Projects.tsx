2a91702a7340d4d9d36f4996d74dd9d5316f63e5
Moyeon <dbqld770@gmail.com>
2023-10-20 03:32:50 +0900

    Project Tab Updated

--- src/components/screens/Projects.tsx
+++ src/components/screens/Projects.tsx
@@ -80,51 +80,43 @@ export default function Projects() {
             - 5인 프로젝트, 개발자 입장에서 의뢰 완수
           </MyText>
           <MyText fontType={FontType.LIGHT} color={Colors.WHITE} fontSize={18} isPretendard={1}>
-            - 앱의 UI/UX 디자인, 프론트엔드 개발
+            - 앱의 UI/UX 디자인, 프론트엔드 개발 담당
+          </MyText>
+          <MyText fontType={FontType.LIGHT} color={Colors.WHITE} fontSize={18} isPretendard={1}>
+            - React Native, Nest.js, Firebase
           </MyText>
           <MyText fontType={FontType.SEMIBOLD} color={Colors.WHITE} fontSize={18}>
-            <A href=""><GitHub className="gitHub"></GitHub><div className="linkLogo"/></A>
-            <A href="https://www.figma.com/proto/Vq8miFw1Ja067gnJLBjhnz/cs350-odokwan?page-id=0%3A1&type=design&node-id=1-2&viewport=46%2C201%2C0.07&t=Hqj1ilU1gKjQagXu-1&scaling=contain&mode=design"><GitHub className="figma"></GitHub><div className="linkLogo fig"/></A>
-            <A href=""><GitHub className="link"></GitHub><div className="linkText">DEMO</div></A>
+            <A href="https://github.com/Gabul99/CS350_CSN"><GitHub className="gitHub"></GitHub><div className="linkLogo"/></A>
+            <A href="https://www.figma.com/file/Fhz4OcD9Igjxj9KUm6MY4o/CS350_Club?type=design&node-id=492%3A488&mode=design&t=Mix6Ix3cMElUqguj-1"><GitHub className="figma"></GitHub><div className="linkLogo fig"/></A>
+            <A href="https://youtu.be/rPf9PiCmifQ"><GitHub className="link"></GitHub><div className="linkText">DEMO</div></A>
           </MyText>
         </ProjBox>
-
-
-
-        <ProjBox title="Personal Quiz" subtitle="2023.03 ~ 2023.04">
+        <ProjBox title="PLOG" subtitle="2023.09 ~ 2023.10">
           <MyText fontType={FontType.LIGHT} color={Colors.WHITE} fontSize={18} isPretendard={1}>
-            - 독서 시간을 기록하고 그에 따른 이미지와 통계를 생성하는 앱
+            - 플로깅 데이터를 합산해 맵으로 보여주는 웹사이트
           </MyText>
           <MyText fontType={FontType.LIGHT} color={Colors.WHITE} fontSize={18} isPretendard={1}>
-            - 5인 프로젝트, 개발자 입장에서 의뢰 작성
+            - 기능이 완성된 상태에서 UI/UX 디자인, 추가
           </MyText>
           <MyText fontType={FontType.LIGHT} color={Colors.WHITE} fontSize={18} isPretendard={1}>
-            - 앱의 UI 담당
+            - React, Firebase
           </MyText>
           <MyText fontType={FontType.SEMIBOLD} color={Colors.WHITE} fontSize={18}>
-            <A href=""><GitHub className="gitHub"></GitHub><div className="linkLogo"/></A>
-            <A href="https://www.figma.com/proto/Vq8miFw1Ja067gnJLBjhnz/cs350-odokwan?page-id=0%3A1&type=design&node-id=1-2&viewport=46%2C201%2C0.07&t=Hqj1ilU1gKjQagXu-1&scaling=contain&mode=design"><GitHub className="figma"></GitHub><div className="linkLogo fig"/></A>
-            <A href=""><GitHub className="link"></GitHub><div className="linkText">VISIT</div></A>
+            <A href="https://www.figma.com/proto/mdbOCBkYIRGUABxTopFHkN/PLOG?page-id=&type=design&node-id=1-3&viewport=448%2C376%2C0.56&t=HbtEcZHQsOigo5Dk-1&scaling=min-zoom&starting-point-node-id=1%3A3&mode=design"><GitHub className="figma"></GitHub><div className="linkLogo fig"/></A>
+            <A href="https://plog-project-jade.vercel.app/"><GitHub className="link"></GitHub><div className="linkText">VISIT</div></A>
           </MyText>
         </ProjBox>
-
-        <MyText fontType={FontType.SEMIBOLD} color={Colors.WHITE} fontSize={20} isPretendard={1}>
-          Personal Quiz Website : https://id311.vercel.app/ 3인 프로젝트, React, Next.js 기반
-        </MyText>
-        <MyText fontType={FontType.SEMIBOLD} color={Colors.WHITE} fontSize={20} isPretendard={1}>
-          ODOKWAN : 4인 프로젝트, 디자인 담당, 클라이언트 롤, 기획
-        </MyText>
-        <MyText fontType={FontType.SEMIBOLD} color={Colors.WHITE} fontSize={20} isPretendard={1}>
-          Club Social Network : https://www.youtube.com/watch?v=rPf9PiCmifQ 프론트엔드 개발자 & UI 디자이너
-        </MyText>
-        <MyText fontType={FontType.SEMIBOLD} color={Colors.WHITE} fontSize={20} isPretendard={1}>
-          PLOG : https://plog-project-jade.vercel.app/ UI 디자이너 & 프론트엔드 적용
-        </MyText>
-        {/* <iframe style={{border: '1px solid rgba(0, 0, 0, 0.1)'}} width={800} height={450} src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FmdbOCBkYIRGUABxTopFHkN%2FPLOG%3Fpage-id%3D0%253A1%26type%3Ddesign%26node-id%3D1-3%26viewport%3D448%252C376%252C0.56%26t%3DenquB28bMCSbNI0m-1%26scaling%3Dscale-down%26starting-point-node-id%3D1%253A3%26mode%3Ddesign" allowFullScreen /> */}
-        <MyText fontType={FontType.SEMIBOLD} color={Colors.WHITE} fontSize={20} isPretendard={1}>
-          KSCIA : UI 디자이너
-        </MyText>
-        
+        <ProjBox title="KSCIA" subtitle="2023.09">
+          <MyText fontType={FontType.LIGHT} color={Colors.WHITE} fontSize={18} isPretendard={1}>
+            - 척수장애인 협회 관리앱
+          </MyText>
+          <MyText fontType={FontType.LIGHT} color={Colors.WHITE} fontSize={18} isPretendard={1}>
+            - 앱의 UI/UX 디자인
+          </MyText>
+          <MyText fontType={FontType.SEMIBOLD} color={Colors.WHITE} fontSize={18}>
+            <A href="https://www.figma.com/proto/wwYGAc4PeE7qjX3Gy7dLQk/KSCIA---Design?page-id=0%3A1&type=design&node-id=26-6237&viewport=-62%2C327%2C0.34&t=yL5qmIzdWidFVsPJ-1&scaling=scale-down&starting-point-node-id=26%3A6237&show-proto-sidebar=1&mode=design"><GitHub className="figma"></GitHub><div className="linkLogo fig"/></A>
+          </MyText>
+        </ProjBox>
       </Box>
     </OneScreen>
   );
